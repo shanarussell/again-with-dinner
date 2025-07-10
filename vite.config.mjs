e55f8128de947692fc3ff08e3cc057ca861e1271
Shana Russell <shana0515@gmail.com>
2025-07-10 17:24:51 -0500

    againnn

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
