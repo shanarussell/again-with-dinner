4a5d5d1cde695fa57b17fce4a15df2eccf4af805
Shana Russell <shana0515@gmail.com>
2025-07-10 18:45:36 -0500

    small update to registration props

--- src/pages/user-registration/components/RegistrationForm.jsx
+++ src/pages/user-registration/components/RegistrationForm.jsx
@@ -5,7 +5,7 @@ import Button from '../../../components/ui/Button';
 import Input from '../../../components/ui/Input';
 import Icon from '../../../components/AppIcon';
 
-const RegistrationForm = () => {
+const RegistrationForm = ({ onRegister, isLoading, disabled }) => {
   const navigate = useNavigate();
   const { signUp } = useAuth();
   const [formData, setFormData] = useState({
